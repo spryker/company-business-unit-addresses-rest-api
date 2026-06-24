1c08a69583e117d3265662bdbcccc532736393f0
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Glue/CompanyBusinessUnitAddressesRestApi/CompanyBusinessUnitAddressesRestApiConfig.php
+++ src/Spryker/Glue/CompanyBusinessUnitAddressesRestApi/CompanyBusinessUnitAddressesRestApiConfig.php
@@ -12,16 +12,22 @@ use Spryker\Glue\Kernel\AbstractBundleConfig;
 class CompanyBusinessUnitAddressesRestApiConfig extends AbstractBundleConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_COMPANY_BUSINESS_UNIT_ADDRESSES = 'company-business-unit-addresses';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTROLLER_RESOURCE_COMPANY_BUSINESS_UNIT_ADDRESSES = 'company-business-unit-addresses-resource';
 
     /**
+     * @api
+     *
      * @deprecated Will be removed with next major release.
      *
      * @var string
@@ -29,31 +35,43 @@ class CompanyBusinessUnitAddressesRestApiConfig extends AbstractBundleConfig
     public const ACTION_COMPANY_BUSINESS_UNIT_ADDRESSES_GET = 'get';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_COMPANY_BUSINESS_UNIT_ADDRESS_NOT_FOUND = '2001';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAIL_COMPANY_BUSINESS_UNIT_ADDRESS_NOT_FOUND = 'Company business unit address not found.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_COMPANY_USER_NOT_SELECTED = '2003';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAIL_COMPANY_USER_NOT_SELECTED = 'Current company user is not set. You need to select the current company user with /company-user-access-tokens in order to access the resource collection.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAIL_RESOURCE_NOT_IMPLEMENTED = 'Resource is not implemented.';
 
     /**
+     * @api
+     *
      * @uses \Spryker\Glue\GlueApplication\GlueApplicationConfig::COLLECTION_IDENTIFIER_CURRENT_USER
      *
      * @var string
